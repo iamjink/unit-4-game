356b3d7406028ceef7212fd3cb4dab63dab9df8d
iamjink <jinijinjin@gmail.com>
2020-01-20 02:09:55 -0800

    further pseudocoding and roughing out functions in javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,7 +13,7 @@ for (var i= lowEnd; i <= highEnd; i++) {
 
 var wins = 0;
 var losses = 0;
-var userScore = 0;
+var userTotal = 0;
 var MatchNumber = 
 
 //random number generator
@@ -23,19 +23,33 @@ var computerNumber = RandomNumberRange[Math.floor(Math.random() * RandomNumberRa
 //pushing random number to match to show on screen
 
 function MatchNumber() {
-    $("#randomNumber").html(computerNumber);
+    $("#randomNumber").text(computerNumber);
+    
 }
 
 MatchNumber();
+console.log(MatchNumber)
 
 //assign numbers to each button
 
-//click listener functions
 
-//when clicking button-one, push the assigned number to memory, then when seconc click happens, add the numbers
-
-//push the sum into totalscore
 
 //when total score equals MatchNumber, then win inreases by one and the number shows in the wins div.
+function userScore(){
+    //onclick listner
+    //add values from clicks
+    return(result)
+    $("#userNumber").text(score)
+} 
+
+userScore.click(){}
 
 //if total score is > than MatchNumber, then loss number increases by one and shows up in the loss div.
+
+if (userTotal === computerNumber){
+wins++;
+$("#wins").text(wins)
+} else if (userTotal > computerNumber) {
+    losses++;
+    $("#losses").text(losses)
+}
