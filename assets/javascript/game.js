19f1fda8616c387233ed40fcad6a9f9677d1f51a
iamjink <jinijinjin@gmail.com>
2020-01-20 11:30:58 -0800

    rewriting random generator to assign random numbers between 1-12 to crystals and random number to match

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,55 @@
-//number choices
+$(document).ready() {
+
+    //random number generator
+
+    var RandomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+
+    //assigning a hidden random value to each crystal between 1 and 12
+
+    var buttonOne = Math.floor(Math.random() *12) + 1;
+    var buttonTwo = Math.floor(Math.random() *12) + 1;
+    var buttonThree = Math.floor(Math.random() *12) + 1;
+    var buttonFour = Math.floor(Math.random() *12) + 1;
 
 
-var RandomNumberRange = [];
+    var wins = 0;
+    var losses = 0;
+    var userTotal = 0;
+    var MatchNumber =
 
-var value = $(this).val();
-var lowEnd = Number(value.split('_')[0]);
-var highEnd = Number(value.split('_')[1]);
 
-for (var i= lowEnd; i <= highEnd; i++) {
-    list.push(i);1
-}
+        //pushing random number to match to show on screen
 
-var wins = 0;
-var losses = 0;
-var userTotal = 0;
-var MatchNumber = 
+        function MatchNumber() {
+            $("#randomNumber").text(computerNumber);
 
-//random number generator
+        }
 
-var computerNumber = RandomNumberRange[Math.floor(Math.random() * RandomNumberRange.length)]; 
+    MatchNumber();
+    console.log(MatchNumber)
 
-//pushing random number to match to show on screen
-
-function MatchNumber() {
-    $("#randomNumber").text(computerNumber);
-    
-}
-
-MatchNumber();
-console.log(MatchNumber)
-
-//assign numbers to each button
+    //assign numbers to each button
 
 
 
-//when total score equals MatchNumber, then win inreases by one and the number shows in the wins div.
-function userScore(){
-    //onclick listner
-    //add values from clicks
-    return(result)
-    $("#userNumber").text(score)
-} 
+    //when total score equals MatchNumber, then win inreases by one and the number shows in the wins div.
+    function userScore() {
+        //onclick listner
+        //add values from clicks
+        return (result)
+        $("#userNumber").text(score)
+    }
 
-userScore.click(){}
+    userScore.click() {}
 
-//if total score is > than MatchNumber, then loss number increases by one and shows up in the loss div.
+    //if total score is > than MatchNumber, then loss number increases by one and shows up in the loss div.
 
-if (userTotal === computerNumber){
-wins++;
-$("#wins").text(wins)
-} else if (userTotal > computerNumber) {
-    losses++;
-    $("#losses").text(losses)
-}
+    if (userTotal === computerNumber) {
+        wins++;
+        $("#wins").text(wins)
+    } else if (userTotal > computerNumber) {
+        losses++;
+        $("#losses").text(losses)
+    }
+
+};
